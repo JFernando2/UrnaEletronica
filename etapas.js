bc13abd576c7bec50124088f76a726fd02423605
José Fernando <jf.marcelino940@gmail.com>
2021-04-29 22:25:41 -0300

    Primeiro Commit

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo:'VEREADOR',
+        numeros: 5,
+        candidatos:[
+            {
+                numero: 38111,
+                nome: 'Leonardo Natal',
+                partido: 'LATOEIRO',
+                fotos:[
+                    {url:'84_2.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: 77222,
+                nome: 'Alex dos Anjos',
+                partido: 'JUJU',
+                fotos:[
+                    {url:'84.jpg', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros: 2,
+        candidatos:[
+            {
+                numero: 28,
+                nome: 'Mauricio Rosa',
+                partido: 'LATOEIRO',
+                vice: 'Marcio',
+                fotos:[
+                    {url:'28.jpg', legenda: 'Prefeito'},
+                    {url:'28_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: 28,
+                nome: 'Luiz Leite',
+                partido: 'JUJU',
+                vice: 'Kiko',
+                fotos:[
+                    {url:'15.jpg', legenda: 'Prefeito'},
+                    {url:'15_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+
+];
